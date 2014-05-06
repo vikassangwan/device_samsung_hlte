afcdc25905bfe7b75125e706ba581b6e322fe208
vikassangwan <vikassangwan@outlook.com>
2014-05-06 13:54:33 -0700

    Update and rename ose.mk to liquid.mk
    liquify

---
+++ liquid.mk
@@ -0,0 +1,13 @@
+$(call inherit-product, device/samsung/hlte/full_hlte.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
+
+# Inherit some common Liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
+# Inherit multicarrier.
+$(call inherit-product, vendor/liquid/config/common_multicarrier.mk)
+
+PRODUCT_DEVICE := hlte
+PRODUCT_NAME := liquid_hlte
